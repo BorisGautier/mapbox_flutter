9d26a5beb9785311decc0666b3aaf7045ea9a696
BorisGautier <tchoukouahaboris@outlook.fr>
2021-09-28 12:28:16 +0100

    Add Example android App & Readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,41 +1,81 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:location/location.dart';
+import 'package:mapbox_flutter_example/custom_marker.dart';
+import 'package:mapbox_flutter_example/offline_regions.dart';
+import 'package:mapbox_flutter_example/place_batch.dart';
 
-import 'package:flutter/services.dart';
-import 'package:mapbox_flutter/mapbox_flutter.dart';
+import 'animate_camera.dart';
+import 'annotation_order_maps.dart';
+import 'full_map.dart';
+import 'line.dart';
+import 'local_style.dart';
+import 'map_ui.dart';
+import 'move_camera.dart';
+import 'page.dart';
+import 'place_circle.dart';
+import 'place_source.dart';
+import 'place_symbol.dart';
+import 'place_fill.dart';
+import 'scrolling_map.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+final List<ExamplePage> _allPages = <ExamplePage>[
+  MapUiPage(),
+  FullMapPage(),
+  AnimateCameraPage(),
+  MoveCameraPage(),
+  PlaceSymbolPage(),
+  PlaceSourcePage(),
+  LinePage(),
+  LocalStylePage(),
+  PlaceCirclePage(),
+  PlaceFillPage(),
+  ScrollingMapPage(),
+  OfflineRegionsPage(),
+  AnnotationOrderPage(),
+  CustomMarkerPage(),
+  BatchAddPage(),
+];
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MapsDemo extends StatelessWidget {
+  //FIXME: Add your Mapbox access token here
+  static const String ACCESS_TOKEN = "YOUR_TOKEN_HERE";
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
+  void _pushPage(BuildContext context, ExamplePage page) async {
+    if (!kIsWeb) {
+      final location = Location();
+      final hasPermissions = await location.hasPermission();
+      if (hasPermissions != PermissionStatus.granted) {
+        await location.requestPermission();
+      }
+    }
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (_) => Scaffold(
+              appBar: AppBar(title: Text(page.title)),
+              body: page,
+            )));
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('MapboxFlutterMaps examples')),
+      body: ListView.builder(
+        itemCount: _allPages.length,
+        itemBuilder: (_, int index) => ListTile(
+          leading: _allPages[index].leading,
+          title: Text(_allPages[index].title),
+          onTap: () => _pushPage(context, _allPages[index]),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(home: MapsDemo()));
 }
